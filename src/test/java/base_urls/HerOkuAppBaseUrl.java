00d33a858fe94dfa9b16fac47093ddd50c22b5ee
Adem Polat <polatadem.2541@gmail.com>
2022-07-18 22:21:05 +0200

    18.07.2022

--- src/test/java/base_urls/HerOkuAppBaseUrl.java
+++ src/test/java/base_urls/HerOkuAppBaseUrl.java
@@ -5,9 +5,14 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
 public class HerOkuAppBaseUrl {
+
     protected RequestSpecification spec;
+
     @Before
     public void setUp(){
-        spec=new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
+
+        spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
+
     }
+
 }
